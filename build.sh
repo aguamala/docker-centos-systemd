1a55c0435f9309d888dd427d158104609919f183
gabo <deobieta@gmail.com>
2016-03-10 14:43:22 -0600

    fix build dir

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-cd build/
+cd build-7/
 
-docker build-7 -t aguamala/centos-systemd:7-${BUILD_NUMBER} .
+docker build -t aguamala/centos-systemd:7-${BUILD_NUMBER} .
 
 DESTINATION=aguamala/centos-systemd:7-${BUILD_NUMBER}
 LATEST_DESTINATION=aguamala/centos-systemd:7
@@ -11,4 +11,3 @@ docker tag aguamala/centos-systemd:7-${BUILD_NUMBER} ${DESTINATION}
 docker push ${DESTINATION}
 docker tag -f aguamala/centos-systemd:7-${BUILD_NUMBER} ${LATEST_DESTINATION}
 docker push ${LATEST_DESTINATION}
-
