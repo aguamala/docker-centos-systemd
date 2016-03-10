104f7b03b85832e1c114e6d41441ea1d40631b07
gabo <deobieta@gmail.com>
2016-03-10 15:05:32 -0600

    centos version var in build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env sh
 
+CENTOS_VERSION=7
+
 cd build-7/
 
-docker build -t aguamala/centos-systemd:7-${BUILD_NUMBER} .
+docker build -t aguamala/centos-systemd:${CENTOS_VERSION} .
 
-DESTINATION=aguamala/centos-systemd:7-${BUILD_NUMBER}
-LATEST_DESTINATION=aguamala/centos-systemd:7
+DESTINATION=aguamala/centos-systemd:${CENTOS_VERSION}
+LATEST_DESTINATION=aguamala/centos-systemd:latest
 
-docker tag aguamala/centos-systemd:7-${BUILD_NUMBER} ${DESTINATION}
+docker tag aguamala/centos-systemd:${CENTOS_VERSION} ${DESTINATION}
 docker push ${DESTINATION}
-docker tag -f aguamala/centos-systemd:7-${BUILD_NUMBER} ${LATEST_DESTINATION}
+docker tag -f aguamala/centos-systemd:${CENTOS_VERSION} ${LATEST_DESTINATION}
 docker push ${LATEST_DESTINATION}
